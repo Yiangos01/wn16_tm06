d0c38af7ff50bcc43a93b7fb3ddcd10e0625beaf
yiangos01 <ygeorg01@cs.ucy.ac.cy>
2016-09-29 18:59:55 +0300

    updated Clinet-Server

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,10 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -9,31 +15,54 @@ import java.util.Enumeration;
 import java.util.Scanner;
 
 public class Client {
-	
-    public static void main(String[] args) throws UnknownHostException, IOException{
-    	int userId = 1;
-    	int port = 450;
-    	String h = "Hello";
-    	Socket cl = new Socket("127.0.0.1",1000);
-    	Scanner scanner = new Scanner(cl.getInputStream());
-    	PrintStream stream = new PrintStream(cl.getOutputStream());
-    	stream.print(h+" "+userId+" "+" "+Ip()+" "+port);
-    	System.out.println(scanner.next());
-    }
-    
-    public static String Ip() {
+	//private static Socket socket;
 
-  	  InetAddress ip;
-  	  try {
+	public static void main(String[] args) throws UnknownHostException, IOException{
 
-  		ip = InetAddress.getLocalHost();
-  		return ip.getHostAddress();
+		int userId=1;
+		int port = 1000;
 
-  	  } catch (UnknownHostException e) {
 
-  		e.printStackTrace();
+		Socket cl = new Socket(InetAddress.getByName("localhost"),1000);
+		int request=0;
+		while(request<300){
+			System.out.println("sdadsa");
+			//send HELLO
+			OutputStream os = cl.getOutputStream();
+			OutputStreamWriter osw = new OutputStreamWriter(os);
+			BufferedWriter bw = new BufferedWriter(osw);
+			String message="HELLO "+userId+" "+" "+Ip()+" "+port+"\n";
+			bw.write(message);
+			
+			bw.flush();
 
-  	  }
-  	  return "";
-  	}
+			//Get the return message from the server
+			InputStream is = cl.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			System.out.println(br.readLine());
+			request++;
+
+		}
+		cl.close();
+	}
+
+
+
+
+	public static String Ip() {
+
+		InetAddress ip;
+		try {
+
+			ip = InetAddress.getLocalHost();
+			return ip.getHostAddress();
+
+		} catch (UnknownHostException e) {
+
+			e.printStackTrace();
+
+		}
+		return "";
+	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,23 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Server{
-  
-    public static void main(String[] args) throws IOException{
-    
-    	ServerSocket serSocket =new ServerSocket(1000);	
-    	Socket s = serSocket.accept();
-    	Scanner scanner = new Scanner(s.getInputStream());
-    	String Hello = scanner.next();
-    	String userId = scanner.next();
-    	String ip = scanner.next();
-    	String port = scanner.next();
-    	PrintStream stream = new PrintStream(s.getOutputStream());
-    	stream.print("Hello");
-    	System.out.println(Hello);
+	
+	public static void main(String[] args) throws IOException{
 
-    }
+			ServerSocket servSocket = new ServerSocket(1000);
+			Socket socket = servSocket.accept();
+			while(true)
+			{
+				
+				
+				InputStream is = socket.getInputStream();
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader br = new BufferedReader(isr);
+				String message = br.readLine();
+				System.out.println(message);
+				String responce[]=message.split(" ");
+				OutputStream os = socket.getOutputStream();
+				OutputStreamWriter osw = new OutputStreamWriter(os);
+				BufferedWriter bw = new BufferedWriter(osw);
+				bw.write("Welcome "+responce[1]+"\n");
+				bw.flush();
+				
+			}
+
+	}
+
 }
